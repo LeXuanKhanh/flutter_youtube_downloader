90133efe16b414401649541c8ab7c18bfa010dbf
lexuankhanh <crazycommander111@gmail.com>
2021-11-04 14:40:48 +0700

    fix. can't run in release mode

--- lib/Extension/StringEx.dart
+++ lib/Extension/StringEx.dart
@@ -8,8 +8,8 @@ extension StringEx on String {
 
     if (Platform.isMacOS) {
       if (this.contains('youtube-dl') || (this.contains('ffmpeg'))) {
-        //return '/usr/local/bin/' + this.substring(2);
-        return 'zsh -c "${this.substring(2)}"';
+        return '/usr/local/bin/' + this.substring(2);
+        //return 'zsh -c "${this.substring(2)}"';
       }
 
       return 'zsh -c "${this}"';
